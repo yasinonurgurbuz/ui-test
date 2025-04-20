3fc04e5c8065291fae009d89d39ca54070d8b093
Yasin Onur Gürbüz <yasinonurgurbuz@yandex.com>
2025-04-20 11:37:49 +0300

    Add browsers setup for chrome and firefox

--- src/test/java/com/insider/journey/BaseTest.java
+++ src/test/java/com/insider/journey/BaseTest.java
@@ -3,16 +3,28 @@ package com.insider.journey;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
     WebDriver driver;
 
+    @Parameters("browser")
     @BeforeClass
-    public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void setup(@Optional("chrome") String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
         driver.manage().window().maximize();
     }
 
